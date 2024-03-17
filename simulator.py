694e04f34a70917e11871ad08fa40721c23ec834
simeontawali <tiwarisa20@gcc.edu>
2024-03-17 16:01:52 -0400

    changes

--- simulator.py
+++ simulator.py
@@ -16,16 +16,40 @@ from travellingSalesman import TravellingSalesman
 
 # Class
 class Simulator:
-    def __init__(self, min_length=100, max_length=200, width=50, sensor_range=5,noise=1.0):
+    def __init__(self, min_length=100, max_length=200, width=50, sensor_range=5.0,noise=1.0):
         self.width = width # y position/axis
         self.sensor_range=sensor_range
         self.noise = noise # we may introduce a noise variable to account for bad measurements that may happen
         self.length = random.randint(min_length, max_length) # x position/axis
         self.grid = np.zeros((self.width, self.length))
-        self.robot_pos = (0, 0)  # Start position
+        self.robot_pos = [width//2,0]  # Start position, middle side of pipe
         self.debris_types = [1, 2, 3]  # three types of debris: metal chips, tape/residue, magnetic chips
+        self.debris_locations = []
         # FIXME: should debris types be "chips","tape","mag"
-        self.mode = 1  # Start with the first cleaning mode
+        self.mode = 0  # Start with the first cleaning mode
+
+    def start_simulation(self):
+        slam = Slam()
+        tsp = TravellingSalesman()
+        dynamicTsp = DynamicTSP()
+        self.populate_debris(0.1)
+
+    def update_slam(self):
+        # update slam
+        # TODO: logic here, update pos and debris?
+        pass
+
+    def tsp_optimization(self):
+        # TODO: finish me
+        pass
+
+    def clean_pipe(self, optimized_path):
+        pass
+
+    def get_debris(self):
+        # TODO: Based on exploration, return a list of debris locations
+        return []
+
 
     def populate_debris(self, density=0.1):
         for _ in range(int(self.width * self.length * density)):
@@ -41,21 +65,19 @@ class Simulator:
         return random.random() * 2.0 - 1.0
     
 
-    # alternate move function assuming move is not accurate
-    # should we use this??
+    # main move function
     def move(self, dx, dy):    
-        x = self.robot_pos[0] + dx + self.rand() * self.noise
-        y = self.robot_pos[1] + dy + self.rand() * self.noise
+        x = self.robot_pos[0] + dx + (self.rand()*2.0-1.0) * self.noise
+        y = self.robot_pos[1] + dy + (self.rand()*2.0-1.0) * self.noise
 
-        if x < 0.0 or x > self.length or y < 0.0 or y > self.width:
-            return False
-        else:
-            self.robot_pos[0] = x
-            self.robot_pos[1] = y
+        # ensure bounds followed
+        if 0 <= x < self.width and 0 <= y < self.length:
+            self.robot_pos = [int(x), int(y)]
             return True
+        return False
 
 
-    # perfect move function??
+    # other move function?? may not work for left/right
     def move_robot(self, direction):
         if direction == 'up' and self.robot_pos[0] > 0:
             self.robot_pos = (self.robot_pos[0] - 1, self.robot_pos[1])
@@ -67,29 +89,7 @@ class Simulator:
             self.robot_pos = (self.robot_pos[0], self.robot_pos[1] - 1)
 
 
-    def robot_vision(self):
-        # lets start by saying robot can see 4 ahead and 2 adjacent grids, we will define acuracy levels
-        # for these grids and say that it knows with certianty what is up close and can predict what is far
-        visible_cells = []
-        # Direct line of sight with 100% accuracy up to 2 grids forward
-        for i in range(1, 3):
-            if self.robot_pos[1] + i < self.length:
-                visible_cells.append((self.robot_pos[0], self.robot_pos[1] + i))
-        
-        # 4 grids forward with 50% accuracy
-        for i in range(3, 5):
-            if self.robot_pos[1] + i < self.length and random.random() < 0.5:
-                visible_cells.append((self.robot_pos[0], self.robot_pos[1] + i))
-        
-        # Adjacent grid in line of sight 2 grids forward with 75% accuracy
-        if self.robot_pos[1] + 2 < self.length and random.random() < 0.75:
-            if self.robot_pos[0] > 0:  # Left side
-                visible_cells.append((self.robot_pos[0] - 1, self.robot_pos[1] + 2))
-            if self.robot_pos[0] < self.width - 1:  # Right side
-                visible_cells.append((self.robot_pos[0] + 1, self.robot_pos[1] + 2))
 
-        return visible_cells
-
-    # TODO: cleaning mode functions
-    # TODO: cleaning functions
+    # TODO: cleaning mode functions, switch between three cleaning modes
+    # TODO: cleaning functions, do cleaning if available
     
--- slam.py
+++ slam.py
@@ -14,8 +14,61 @@ References:
 https://www.geeksforgeeks.org/simultaneous-localization-and-mapping/
 
 """
+import random
 
 class Slam:
-    def __init__(self):
-        self.objects = []
-        self.num_objects = 0
+    def __init__(self, grid, start_pos):
+        self.grid = grid
+        self.robot_pos = start_pos
+        self.visited = set()
+        self.to_visit = [start_pos]
+
+    def explore(self):
+        while self.to_visit:
+            current_pos = self.to_visit.pop(0)
+            if current_pos in self.visited:
+                continue
+            self.visited.add(current_pos)
+            for direction in [(0,1), (1,0), (0,-1), (-1,0)]: # Explore in all four directions
+                next_pos = (current_pos[0] + direction[0], current_pos[1] + direction[1])
+                if 0 <= next_pos[0] < self.grid.shape[0] and 0 <= next_pos[1] < self.grid.shape[1]:
+                    self.to_visit.append(next_pos)
+                    # Simulate sensor data collection here if needed
+        # Update the grid or robot's knowledge base with explored data
+                    
+
+
+    def robot_vision(self, robot_pos, length, width):
+        # lets start by saying robot can see 4 ahead and 2 adjacent grids, we will define acuracy levels
+        # for these grids and say that it knows with certianty what is up close and can predict what is far
+        visible_cells = []
+        # Direct line of sight with 100% accuracy up to 2 grids forward
+        for i in range(1, 3):
+            if self.robot_pos[1] + i < length:
+                visible_cells.append((robot_pos[0], robot_pos[1] + i))
+        
+        # 4 grids forward with 50% accuracy
+        for i in range(3, 5):
+            if robot_pos[1] + i < length and random.random() < 0.5:
+                visible_cells.append((robot_pos[0], robot_pos[1] + i))
+        
+        # Adjacent grid in line of sight 2 grids forward with 75% accuracy
+        if robot_pos[1] + 2 < length and random.random() < 0.75:
+            if robot_pos[0] > 0:  # Left side
+                visible_cells.append((robot_pos[0] - 1, robot_pos[1] + 2))
+            if robot_pos[0] < width - 1:  # Right side
+                visible_cells.append((robot_pos[0] + 1, robot_pos[1] + 2))
+
+        return visible_cells
+
+    def sense_debris(self, robot_pos, sensor_range, debris_locations, noise):
+        measurements = []
+        for index, (dx, dy) in enumerate(debris_locations):
+            distance_x = dx - robot_pos[0]
+            distance_y = dy - robot_pos[1]
+            distance_x += (random.random() * 2.0 - 1.0) * noise
+            distance_y += (random.random() * 2.0 - 1.0) * noise
+
+            if abs(distance_x) < sensor_range and abs(distance_y) < sensor_range:
+                measurements.append([index, distance_x, distance_y])
+        return measurements
