5939dd2a1962d6b524de198da85d1f5e15e64f19
noYuen <yuennr20@gcc.edu>
2024-03-17 19:18:15 -0400

    dynamicTSP returns a list of coords as a path to take

--- dynamicTSP.py
+++ dynamicTSP.py
@@ -11,29 +11,48 @@ Version: 0.0.1
 References:
 https://www.geeksforgeeks.org/travelling-salesman-problem-using-dynamic-programming/
 """
+
+import sys
 import numpy as np
 class DynamicTSP:
     def __init__(self, distances):
         self.distances = distances  # Distance matrix
         self.n = len(distances)  # Number of nodes/cities
-        self.dp = [[np.inf for _ in range(self.n)] for __ in range(1 << self.n)]
-        self.parent = [[-1 for _ in range(self.n)] for __ in range(1 << self.n)]
+        #self.dp = [[np.inf for _ in range(self.n)] for __ in range(1 << self.n)]
+        #self.parent = [[-1 for _ in range(self.n)] for __ in range(1 << self.n)]
 
-    def dynamic_tsp(self):
+    def dynamic_tsp(distances, debris_locations):
         # Have a base case maybe, start from the first city
         # DP to compute minimum cost
+        shallow_dis = distances
         # Reconstruct path
-        path = None
+        node_path = [0]
         cost = 0
+        for liss in shallow_dis:
+            for dis in liss:
+                to_next = liss.index(min(liss))
+                if(to_next in node_path):
+                    liss[to_next] = sys.maxsize
+                else:
+                    cost += liss[to_next]
+                    node_path.append(to_next)
+                    break
+        node_path.append(0)
+
+        path = []
+
+        for node in node_path:
+            path.append(debris_locations[node])
+
+        print(path)
+        print(cost)
         return path, cost
+    
+    
+    
+    
 
 
-    @staticmethod
-    def calculate_distances(locations):
-        # Calculate Euclidean distances between locations
-        # TODO: finish me
-        distances = []
-        return distances
 
 
 
